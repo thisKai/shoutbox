bcabe16fc1d35f14afbe681d647daa0ab4fa2983
Kai Patrick <thiskaiguy@gmail.com>
2017-09-22 19:19:19 +0100

    Fix the output filename in the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './client/index.js',
     output: {
-        filename: 'client',
+        filename: 'client.js',
         path: path.resolve(__dirname, 'dist'),
     },
 };
