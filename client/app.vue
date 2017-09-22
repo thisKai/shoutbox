c6d27aac43282360f5f4f7e9fc99c7bcb399b5bb
Kai Patrick <thiskaiguy@gmail.com>
2017-09-22 21:01:57 +0100

    Create a skeleton chat view

--- client/app.vue
+++ client/app.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Hello world!</div>
+    <main id="app">
+        <div>
+            <div v-for="message in messages" :key="message"></div>
+        </div>
+        <footer>
+            <input/>
+            <button>Send</button>
+        </footer>
+    </main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: [],
+        };
+    },
+};
+</script>
