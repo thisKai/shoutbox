c49d3867cf0e0e0777e90a6d88108121b271c7fd
Kai Patrick <thiskaiguy@gmail.com>
2017-09-27 18:36:35 +0100

    Fix the database reset trigger

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ let database;
 async function open() {
   console.log('opening database');
   const db = await sqlite.open('./.data/db.sqlite');
-  if (process.env.SHOUTBOX_RESET_DB === 1) {
+  if (process.env.SHOUTBOX_RESET_DB === '1') {
     await db.migrate({force: 'last'});
   }
   return db;
