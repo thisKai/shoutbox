09f6b062d33707b5c42c79ab6bf6171425ebc1a4
Kai Patrick <thiskaiguy@gmail.com>
2017-09-27 17:59:50 +0100

    Insert the NODE_ENV environment variable into the client side javascript

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,11 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
   ],
   devtool: 'inline-source-map',
 };
