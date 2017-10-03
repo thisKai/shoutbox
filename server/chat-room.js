983bb5d4f300f4fcaf5d8dd5d7d99b0d811d162d
Kai Patrick <thiskaiguy@gmail.com>
2017-10-03 23:09:59 +0100

    Save the chat room id and allow retrieving it

--- server/chat-room.js
+++ server/chat-room.js
@@ -1,8 +1,10 @@
+let chatRoomId;
 async function initializeChatRoom(db) {
   const result = await db.run('INSERT INTO ChatRooms DEFAULT VALUES');
-  const chatRoomId = result.stmt.lastID;
+  chatRoomId = result.stmt.lastID;
 }
 
 module.exports = {
   initializeChatRoom,
+  chatRoomId: () => chatRoomId,
 };
