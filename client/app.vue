c28dd55daa68f7b3df6ae69b2ee53c65cb74bde5
Kai Patrick <thiskaiguy@gmail.com>
2017-10-05 13:22:24 +0100

    Extract the chat view into its own component

--- client/app.vue
+++ client/app.vue
@@ -1,16 +1,6 @@
 <template>
 <main id="app" class="root">
-  <div class="message-list" v-chat-scroll>
-    <div v-for="(message, index) in messages" :key="index" class="message-item">
-      <loading-box :loading="messageIsSending(message)">
-        <pre>{{ message.content }}</pre>
-      </loading-box>
-    </div>
-  </div>
-  <footer class="message-form">
-    <chat-input class="message-input" v-model="newMessage" @submit="sendMessage" placeholder="Type your message"></chat-input>
-    <button class="send-button" @click="sendMessage">Send</button>
-  </footer>
+  <chat-view class="chat-view" :messages="messages" @send="sendMessage"/>
 </main>
 </template>
 
@@ -19,19 +9,16 @@ import {
   REFRESH_MESSAGES,
   SEND_MESSAGE,
 } from '../socket-messages';
-import LoadingBox from './loading-box.vue'
-import ChatInput from './chat-input.vue';
+import ChatView from './chat-view.vue';
 
 export default {
   components: {
-    LoadingBox,
-    ChatInput,
+    ChatView,
   },
   data() {
     return {
       socket: null,
       messages: [],
-      newMessage: '',
     };
   },
   mounted() {
@@ -63,21 +50,18 @@ export default {
           break;
       }
     },
-    sendMessage() {
-      if (this.newMessage.trim() === '') {
+    sendMessage(newMessage) {
+      const message = newMessage.trim();
+      if (message === '') {
         return;
       }
+      this.messages.push({
+        content: message,
+      });
       this.socket.send(JSON.stringify({
         type: SEND_MESSAGE,
-        content: this.newMessage.trim(),
+        content: message,
       }));
-      this.messages.push({
-        content: this.newMessage.trim(),
-      });
-      this.newMessage = '';
-    },
-    messageIsSending(message){
-      return !message.hasOwnProperty('_id');
     },
   },
 };
@@ -85,71 +69,9 @@ export default {
 
 <style scoped>
 .root {
-  display: flex;
-  flex-direction: column;
   height: 100%;
 }
-
-.message-list {
-  flex-grow: 1;
-  overflow: auto;
-  background: #444;
-}
-
-.message-item {
-  background-color: #999;
-  padding: 8px 16px;
-  margin: 8px;
-  border-radius: 8px;
-}
-
-.message-item pre {
-  margin: 0;
-}
-
-.message-form {
-  display: flex;
-  background-color: #000;
-  flex-shrink: 0;
-}
-
-.message-input {
-  flex-grow: 1;
-}
-
-.message-input::-webkit-input-placeholder {
-  /* Chrome/Opera/Safari */
-  color: #CCC;
-}
-
-.message-input::-moz-placeholder {
-  /* Firefox 19+ */
-  color: #CCC;
-}
-
-.message-input:-ms-input-placeholder {
-  /* IE 10+ */
-  color: #CCC;
-}
-
-.message-input:-moz-placeholder {
-  /* Firefox 18- */
-  color: #CCC;
-}
-
-.send-button {
-  background: none;
-  border: none;
-  padding: 8px 16px;
-  color: #FFF;
-}
-
-.send-button:hover {
-  background-color: rgba(255, 255, 255, 0.25);
-}
-
-.send-button:hover:active {
-  color: #000;
-  background-color: rgba(255, 255, 255, 0.5);
+.chat-view {
+  height: 100%;
 }
 </style>
---
+++ client/chat-view.vue
@@ -0,0 +1,118 @@
+<template>
+<div class="wrapper">
+  <div class="message-list" v-chat-scroll>
+    <div v-for="(message, index) in messages" :key="index" class="message-item">
+      <loading-box :loading="messageIsSending(message)">
+        <pre>{{ message.content }}</pre>
+      </loading-box>
+    </div>
+  </div>
+  <footer class="message-form">
+    <chat-input class="message-input" v-model="newMessage" @submit="sendMessage" placeholder="Type your message"></chat-input>
+    <button class="send-button" @click="sendMessage">Send</button>
+  </footer>
+</div>
+</template>
+
+<script>
+import LoadingBox from './loading-box.vue'
+import ChatInput from './chat-input.vue';
+
+export default {
+  components: {
+    LoadingBox,
+    ChatInput,
+  },
+  data() {
+    return {
+      newMessage: '',
+    };
+  },
+  props: {
+    messages: Array,
+  },
+  methods: {
+    sendMessage() {
+      const message = this.newMessage.trim();
+      if (message === '') {
+        return;
+      }
+      this.$emit('send', message);
+      this.newMessage = '';
+    },
+    messageIsSending(message){
+      return !message.hasOwnProperty('_id');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+}
+
+.message-list {
+  flex-grow: 1;
+  overflow: auto;
+  background: #444;
+}
+
+.message-item {
+  background-color: #999;
+  padding: 8px 16px;
+  margin: 8px;
+  border-radius: 8px;
+}
+
+.message-item pre {
+  margin: 0;
+}
+
+.message-form {
+  display: flex;
+  background-color: #000;
+  flex-shrink: 0;
+}
+
+.message-input {
+  flex-grow: 1;
+}
+
+.message-input::-webkit-input-placeholder {
+  /* Chrome/Opera/Safari */
+  color: #CCC;
+}
+
+.message-input::-moz-placeholder {
+  /* Firefox 19+ */
+  color: #CCC;
+}
+
+.message-input:-ms-input-placeholder {
+  /* IE 10+ */
+  color: #CCC;
+}
+
+.message-input:-moz-placeholder {
+  /* Firefox 18- */
+  color: #CCC;
+}
+
+.send-button {
+  background: none;
+  border: none;
+  padding: 8px 16px;
+  color: #FFF;
+}
+
+.send-button:hover {
+  background-color: rgba(255, 255, 255, 0.25);
+}
+
+.send-button:hover:active {
+  color: #000;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
