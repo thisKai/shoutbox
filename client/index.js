f5bafb34865fefd2e126f1915356892622857522
Kai Patrick <thiskaiguy@gmail.com>
2017-10-10 21:53:17 +0100

    Install vuex and create a skeleton store

--- client/index.js
+++ client/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueChatScroll from 'vue-chat-scroll';
 
 import App from './app.vue';
+import store from './store';
 
 Vue.use(VueChatScroll);
 
 new Vue({
   el: '#app',
+  store,
   render(h) {
     return h(App);
   },
